76d4c1b4fd3c93c7b629109b30850c9dec25db5d
Ali Khazem <ST10032904@vcconnect.edu.za>
2023-04-13 20:11:30 +0200

    Complete Program, need to add abstraction into the program|

---
+++ abstraction/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace abstraction
+{
+    internal class Class1
+    {
+        private double topBase = 0;
+        private double bottomBase = 0;
+        private double leftBase = 0;
+        public Class1(Double topBase, Double lowerBase, Double height)
+        {
+            this.topBase = topBase;
+            this.bottomBase = lowerBase;
+            this.leftBase = height;
+        }
+
+        public String calculateArea()
+        {
+            double result  = (topBase + bottomBase) / 2 * leftBase;
+            return "The area is " + result;
+        }
+    }
+}
---
+++ abstraction/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+
+namespace abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Which object would you like to create (Recatangle, Cirlce, Trapezium)? please type your answer in lowercase");
+            String input = Console.ReadLine();
+            if (input.Equals("triangle" , StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("What is the width of the triangle?");
+                double width = Double.Parse(Console.ReadLine());
+                Console.WriteLine("What is the height of the tiangle?");
+                double height = Double.Parse(Console.ReadLine());
+                Console.WriteLine(height * width);
+            }
+            else if (input.Equals("circle" ,  StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("What is the readius?");
+                double radius = Double.Parse(Console.ReadLine());
+                Console.WriteLine(radius * 3.141);
+            }
+            else if (input.Equals("trapezium" , StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("what is the top base of the trapezium");
+                double topBase = Double.Parse(Console.ReadLine());
+                Console.WriteLine("what is the bottom base of the trapezium");
+                double bottomBase = Double.Parse(Console.ReadLine());
+                Console.WriteLine("what is the height of the trapezium");
+                double height = Double.Parse(Console.ReadLine());
+                Class1 trap = new Class1(topBase, bottomBase, height);
+                Console.WriteLine(trap.calculateArea());
+            }
+            else
+            {
+                Console.WriteLine("Please enter either: triangle, circle, or trapezium");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
